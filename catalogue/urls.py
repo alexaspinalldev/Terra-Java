b9d51c258ca0f25b53c34ebd37414c0e0b53170e
Alexander Aspinall <alex.aspinall@blueyonder.co.uk>
2025-01-13 13:10:43 +0000

    Add coffee detail view

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('catalogue/', views.CoffeeList.as_view(), name='catalogue'),
+    path('catalogue/<int:product_ID>/', views.coffee_detail, name='coffee_detail'),
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Coffee
 
@@ -7,3 +7,13 @@ class CoffeeList(generic.ListView):
     queryset = Coffee.objects.filter(listing_approved=True)
     template_name = "coffee_list.html"
     paginate_by = 12
+
+def coffee_detail(request, product_ID):
+    queryset = Coffee.objects.filter(listing_approved=True)
+    coffee = get_object_or_404(queryset, product_ID=product_ID)
+
+    return render(
+        request,
+        "coffee_detail.html",
+        {"coffee": coffee},
+    )
