b1d2ad17c4b73413c29d40053ed284863e1ac4c2
Alexander Aspinall <alex.aspinall@blueyonder.co.uk>
2025-01-14 01:14:01 +0000

    Add buttons to coffe detail, and create data-load attribute to tag slow loading links and show a loading modal

---
+++ static/js/loading.js
@@ -0,0 +1,9 @@
+const loaders = document.querySelectorAll('[data-load]');
+const loadingModal = new bootstrap.Modal(document.getElementById('loadingModal'));
+
+loaders.forEach(loader => {
+    loader.addEventListener("click", (e) => {
+        loadingModal.show();
+        console.log("Modal shown");
+    });
+});
---
+++ staticfiles/js/loading.js
@@ -0,0 +1,5 @@
+loaders = document.querySelector('[data-load]')
+// loadingModal = getElementById("loadingModal")
+loaders.addEventListener("click", () => {
+    $("#myModal").loadingModal()
+})
