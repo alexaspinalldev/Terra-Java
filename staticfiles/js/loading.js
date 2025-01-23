98b6c966bb9a70d277c1e699322c3f21451eecfd
Alexander Aspinall <alex.aspinall@blueyonder.co.uk>
2025-01-23 14:15:11 +0000

    Resolve issue with loading modal and cmd clicking

--- staticfiles/js/loading.js
+++ staticfiles/js/loading.js
@@ -3,6 +3,10 @@ const loadingModal = new bootstrap.Modal(document.getElementById('loadingModal')
 
 loaders.forEach(loader => {
     loader.addEventListener("click", (e) => {
+        if (e.metaKey || e.ctrlKey) {
+            return;
+        }
+
         loadingModal.show();
     });
 });
