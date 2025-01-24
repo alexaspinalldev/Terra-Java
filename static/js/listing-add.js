d353588ec4c00c939fd825a246f680675cf91e97
Alexander Aspinall <alex.aspinall@blueyonder.co.uk>
2025-01-24 12:53:37 +0000

    JShint corrections

--- static/js/listing-add.js
+++ static/js/listing-add.js
@@ -1,6 +1,6 @@
 // Add listing
-const addBtn = document.getElementById("addBtn")
+const addBtn = document.getElementById("addBtn");
 const addModal = new bootstrap.Modal(document.getElementById("addModal"));
 addBtn.addEventListener("click", () => {
-    addModal.show()
-})
+    addModal.show();
+});
